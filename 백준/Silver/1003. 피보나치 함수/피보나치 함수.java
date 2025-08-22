8866c8c213b881408e789f4a58583f17deb774eb
김정환 <103404957+rlawjdghksdlqslek@users.noreply.github.com>
2025-08-22 10:06:18 +0900

    [Silver III] Title: 피보나치 함수, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static Integer[] fibonacci(int n) {
+        if (dp[n][0] == null || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
+        }
+        return dp[n];
+    }
+}
