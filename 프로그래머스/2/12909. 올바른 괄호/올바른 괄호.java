f0617f3cf4c4620a4e6c9346417ea2da98da90f2
김정환 <103404957+rlawjdghksdlqslek@users.noreply.github.com>
2025-09-09 09:03:27 +0900

    [level 2] Title: 올바른 괄호, Time: 9.13 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int sum = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                sum++;
+            } else if (s.charAt(i) == ')') {
+                sum--;
+            }
+            if (sum < 0) {
+                answer = false;
+                break;
+            }
+        }
+
+        if (sum != 0) {
+            answer = false;
+        }
+        return answer;
+    }
+}
