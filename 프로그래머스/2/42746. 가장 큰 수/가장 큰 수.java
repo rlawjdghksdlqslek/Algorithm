c2d5008b22c9de74b08e510712b518ed0cbd8e77
김정환 <103404957+rlawjdghksdlqslek@users.noreply.github.com>
2025-09-15 10:04:25 +0900

    [level 2] Title: 가장 큰 수, Time: 228.82 ms, Memory: 145 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strNumbers = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strNumbers[0].equals("0")) return "0";
+
+        return String.join("", strNumbers);
+    }
+}
